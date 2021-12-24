206b15ef53f061056b89ea8ea0dcf661dc0ee349
John Salinas <salinasjohn257@gmail.com>
2021-12-23 19:18:22 -0800

    add container for tab title and future buttons

--- popup.js
+++ popup.js
@@ -2,12 +2,29 @@
 function logTabs(tabs) {
 
     for(let tab of tabs) {
-        let container = document.getElementById("url_container");
-        let newParagraph = document.createElement("p");
-        newParagraph.innerHTML = `${tab.url}`;
-        container.appendChild(newParagraph);
+        let tabBox = makeTabContainer(tab);
+        showTabTitle(tabBox, tab);
+        
     }
 };
+
+function makeTabContainer(tab) {
+    let container = document.getElementById('url_container');
+    let newTabBox = document.createElement('div');
+    container.appendChild(newTabBox);
+    return newTabBox;
+}
+
+function showTabTitle(tabBox, tab) {
+    let newParagraph = document.createElement('p');
+    let titleText = shortenTitle(tab.title);
+    newParagraph.innerHTML = `${titleText}`;
+    tabBox.appendChild(newParagraph);
+}
+
+function shortenTitle(title) {
+    return title.slice(0,30) + "...";
+}
 
 function onError(error) {
     console.log(`Error: ${error}`);
